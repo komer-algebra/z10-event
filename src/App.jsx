4bef50e3fc1c3ec7bc40d5e134718e1ace1e1964
Komer @ Algebra <komersmile@gmail.com>
2023-09-18 18:40:36 +0200

    promjena zadataka od prosli puta

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
 import './App.css'
-import { Component } from 'react'
+import { useState } from 'react'
 import { UserClass, UserFunction, UserChildren } from './user'
 
-const users = [
-  { name: 'Ivan', years: 30, },
-  { name: 'Marko', years: 35 },
-  { name: 'Ana', years: 25 },
-];
+function App() {
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <h1>State</h1>
-        <UserClass name={users[0].name} years={users[0].years} />
-        <UserFunction name={users[1].name} years={users[1].years} />
-        <UserChildren name={users[2].name} years={users[2].years}>
-          a hobi mi je programiranje...
-        </UserChildren>
-      </>
-    )
+  const initialUsers = [
+    { name: 'Ivan', years: 30 },
+    { name: 'Marko', years: 35 },
+    { name: 'Ana', years: 25 },
+  ]
+
+  const [users, setUsers] = useState(initialUsers)
+
+  const tekst = "Proizvoljan tekst"
+
+
+  const uvecajGodine = () => {
+    setUsers(prevState => {
+      const updatedUsers = prevState.map(user => {
+        return { ...user, years: user.years + 1 }
+      })
+      return updatedUsers
+    })
   }
+
+  return (
+    <>
+      <h1>State</h1>
+      <UserClass name={users[0].name} years={users[0].years} />
+      <UserFunction name={users[1].name} years={users[1].years} />
+      <UserChildren name={users[2].name} years={users[2].years}>
+        {tekst}
+      </UserChildren>
+      <button onClick={uvecajGodine}>Uvecaj godine</button>
+    </>
+  )
 }
 
 export default App
